46c2ff9af3d4ef69e4265c2c57e2c3197468374e
Marat Kashaev <marat@gomagis.com>
2020-07-10 15:06:00 +0300

    ADD routes and controllers

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,11 +1,12 @@
 module.exports.login = (req, res) => {
   res.status(200).json({
-    message: 'login'
-  })
-}
+    login: req.body.email,
+    password: req.body.password
+  });
+};
 
 module.exports.register = (req, res) => {
   res.status(200).json({
     message: 'register'
-  })
-}
+  });
+};
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/auth');
 
-router.get('/login', controller.login);
-router.get('/register', controller.register);
+router.post('/login', controller.login);
+router.post('/register', controller.register);
 
 module.exports = router;
