1070e9c93306ce7088a1cc6a98aa5e6a236b39dd
Marat Kashaev <marat@gomagis.com>
2020-07-14 17:49:22 +0300

    ADD order logic

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -1,7 +1,58 @@
-module.exports.getAll = (req, res) => {
+const Order = require('../modules/Order');
+const errorHandler = require('../utils/errorHandler');
 
+
+module.exports.getAll = async (req, res) => {
+  const { offset, limit } = req.query;
+  const query = {
+    user: req.user.id,
+  };
+
+  if (req.query.start) {
+    query['date'] = {
+      $gte: req.query.start
+    }
+  }
+
+
+  if (req.query.end) {
+    query['date'] = {
+      $lte: req.query.end
+    }
+  }
+
+  if (req.query.order) {
+    query.order = +req.query.order;
+  }
+
+  try {
+    const orders = await Order
+      .find(query)
+      .sort({ date: -1 })
+      .skip(+offset)
+      .limit(+limit);
+    
+      res.status(200).json(orders);
+  } catch (err) {
+    errorHandler(res, err);
+  }
 }
 
-module.exports.create = (req, res) => {
-  
+module.exports.create = async (req, res) => {
+  try {
+    const lastOrder = await Order.findOne({ user: req.user.id }).sort({ date: -1 });
+    const maxOrder = lastOrder ? lastOrder.order : 0;
+
+    const order = new Order({
+      list: req.body.list,
+      user: req.user.id,
+      order: maxOrder + 1,
+    });
+    await order.save();
+    res.status(201).json({
+      message: 'Created',
+    });
+  } catch (err) {
+    errorHandler(res, err);
+  }
 }
